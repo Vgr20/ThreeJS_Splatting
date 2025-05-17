af5021b344b63aaae5ec90df8e837f3876cc0c8c
Vishagar Arunan <vishagara.20@uom.lk>
2025-05-17 13:41:25 +0530

    update

--- src/views/viewer_independence_square_XR.tsx
+++ src/views/viewer_independence_square_XR.tsx
@@ -19,7 +19,7 @@ const ViewerGaneshismLobbyXR = () => {
     viewer
       .addSplatScene(path, {
         scale: [2, 2, 2],
-        position: [0, 0, 0],
+        position: [0, -0.25, 0],
         // rotation: [Math.PI, 0, 0],
         rotation: [1, 0, 0, 0],
       })
