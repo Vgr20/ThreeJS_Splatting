46d4a4819a91b0c7ed62cc8251bc6c74c199bd4d
Vishagar Arunan <vishagara.20@uom.lk>
2025-05-18 18:03:41 +0530

    Update viewer_ganeshism_lobby_XR.tsx

--- src/views/viewer_ganeshism_lobby_XR.tsx
+++ src/views/viewer_ganeshism_lobby_XR.tsx
@@ -12,10 +12,10 @@ const ViewerGaneshismLobbyXR = () => {
       initialCameraLookAt: [1, 1, 1],
       // initialCameraLookAt: [-1.36147, 0.01385, -1.93057],
       webXRMode: GaussianSplats3D.WebXRMode.VR,
-      // webXRSessionInit: {
-      //   requiredFeatures: ["bounded-floor"], // Try also "bounded-floor"
-      //   optionalFeatures: ["hit-test", "light-estimation"], // optional
-      // },
+      webXRSessionInit: {
+        requiredFeatures: ["bounded-floor"], // Try also "bounded-floor"
+        optionalFeatures: ["hit-test", "light-estimation"], // optional
+      },
       container: containerRef.current,
     });
 
