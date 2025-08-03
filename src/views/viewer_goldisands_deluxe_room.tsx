a62f6181087db5c644abb0658f9c7708c42dc9f3
Hashiru Pramuditha <aahpramuditha1212@gmail.com>
2025-08-03 17:07:39 +0530

    changed the scene orientation

--- src/views/viewer_goldisands_deluxe_room.tsx
+++ src/views/viewer_goldisands_deluxe_room.tsx
@@ -67,7 +67,7 @@ function ViewerGoldisandsDeluxeRoom() {
         showLoadingUI: true,
         position: [0, 0.5, 0],
         // rotation: [1, 0, 0, 0],
-        rotation: [1, 0, Math.sin(Math.PI / 36), Math.cos(Math.PI / 36)],
+        rotation: [1, 0, Math.sin(Math.PI / 36), 0],
         scale: [1, 1, 1],
       })
       .then(() => {
