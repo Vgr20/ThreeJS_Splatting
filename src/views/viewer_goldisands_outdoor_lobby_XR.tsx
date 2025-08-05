7b47a9391d5123a71415aa4d2b94316850525ba3
Nazar Fathima Saeedha <111122103+Saeedha-N@users.noreply.github.com>
2025-08-05 07:58:06 +0530

    Update viewer_goldisands_outdoor_lobby_XR.tsx

--- src/views/viewer_goldisands_outdoor_lobby_XR.tsx
+++ src/views/viewer_goldisands_outdoor_lobby_XR.tsx
@@ -18,7 +18,7 @@ const ViewerGoldisandsOutdoorLobbyXR = () => {
     viewer
       .addSplatScene(path, {
         scale: [0.9, 0.9, 0.9],
-        position: [0, -0.1, 0],
+        position: [0, 0.3, 0],
         // rotation: [Math.PI, 0, 0],
         rotation: [1, 0, 0, 0],
       })
